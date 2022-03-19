20b56f4712deab0fe8f429a67edf583a8b97275b
--replace-all <2361575m@student.gla.ac.uk>
2022-03-19 03:00:10 +0000

    added dropdown fields for registration

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,37 +1,38 @@
+//just some random values i added 
 var subjectObject = {
-    "Front-end": {
-        "HTML": ["Links", "Images", "Tables", "Lists"],
-        "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
-        "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]
+    "United Kingdom": {
+        "Engineering": ["Electrical Engineering", "Chemical Engineering", "Biomedical Engineering", "Mechanical Engineering"],
+        "Sciences": ["Biochemistry", "Physics", "Pharmacology", "Chemistry"],
+        "Business": ["Accounting", "Finance", "Marketing", "Management"]
     },
-    "Back-end": {
-        "PHP": ["Variables", "Strings", "Arrays"],
-        "SQL": ["SELECT", "UPDATE", "DELETE"]
+    "Canada": {
+        "History": ["Social History", "Political History", "Economic History"],
+        "Arts": ["Film", "Music", "Theatre"]
     }
 }
 window.onload = function () {
+    var countrySel = document.getElementById("country");
+    var departmentSel = document.getElementById("department");
     var subjectSel = document.getElementById("subject");
-    var topicSel = document.getElementById("topic");
-    var chapterSel = document.getElementById("chapter");
     for (var x in subjectObject) {
-        subjectSel.options[subjectSel.options.length] = new Option(x, x);
+        countrySel.options[countrySel.options.length] = new Option(x, x);
     }
-    subjectSel.onchange = function () {
-        //empty Chapters- and Topics- dropdowns
-        chapterSel.length = 1;
-        topicSel.length = 1;
+    countrySel.onchange = function () {
+        //empty Subjects- and Dropdowns- dropdowns
+        subjectSel.length = 1;
+        departmentSel.length = 1;
         //display correct values
         for (var y in subjectObject[this.value]) {
-            topicSel.options[topicSel.options.length] = new Option(y, y);
+            departmentSel.options[departmentSel.options.length] = new Option(y, y);
         }
     }
-    topicSel.onchange = function () {
-        //empty Chapters dropdown
-        chapterSel.length = 1;
+    departmentSel.onchange = function () {
+        //empty Subjects dropdown
+        subjectSel.length = 1;
         //display correct values
-        var z = subjectObject[subjectSel.value][this.value];
+        var z = subjectObject[countrySel.value][this.value];
         for (var i = 0; i < z.length; i++) {
-            chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
+            subjectSel.options[subjectSel.options.length] = new Option(z[i], z[i]);
         }
     }
 }
