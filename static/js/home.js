cc4d4613efc8afa8ce7e5d5723b4375486cd208e
Ayush-Sharma-1 <2634592S@student.gla.ac.uk>
2022-03-21 04:03:38 +0000

    Half changes for home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,45 +1,20 @@
-
-
-function getCountry() {
-    //country_val = document.getElementById("country-dropdown").value; 
-        let dropdown, table, rows, cells, country, filter;
-        dropdown = document.getElementById("country-dropdown");
-        table = document.getElementById("unitable");
-        rows = table.getElementsByTagName("tr");
-        filter = dropdown.value;
-        for (let row of rows) { 
-          cells = row.getElementsByTagName("td");
-          country = cells[1] || null; 
-          if (filter === "all" || !country || (filter === country.textContent)) {
-            row.style.display = ""; // shows this row
-          }
-          else {
-            row.style.display = "none"; // hides this row
-          }
-        }
-     }
-
-function getDepartment() {
-    department_val = document.getElementById("department-dropdown").value;
-}
-
-
     $(document).ready(function () {
         $("#country-dropdown,#department-dropdown").on("change", function () {
             var country = $('#country-dropdown').find("option:selected").val();
             var dept = $('#department-dropdown').find("option:selected").val();
+            console.log("This is ready")
             SearchData(country, dept)
         });
     });
-    function SearchData(country, age) {
-        if (country.toUpperCase() == 'ALL' && age.toUpperCase() == 'ALL') {
-            $('#table11 tbody tr').show();
+    function SearchData(country, dept) {
+        if (country.toUpperCase() == 'ALL' && dept.toUpperCase() == 'ALL') {
+            $('#unitable tbody tr').show();
         } else {
-            $('#table11 tbody tr:has(td)').each(function () {
+            $('#unitable tbody tr:has(td)').each(function () {
                 var rowCountry = $.trim($(this).find('td:eq(1)').text());
-                var rowAge = $.trim($(this).find('td:eq(2)').text());
-                if (country.toUpperCase() != 'ALL' && age.toUpperCase() != 'ALL') {
-                    if (rowCountry.toUpperCase() == country.toUpperCase() && rowAge == age) {
+                var rowDept = $.trim($(this).find('td:eq(2)').text());
+                if (country.toUpperCase() != 'ALL' && dept.toUpperCase() != 'ALL') {
+                    if (rowCountry.toUpperCase() == country.toUpperCase() && rowDept == dept) {
                         $(this).show();
                     } else {
                         $(this).hide();
@@ -52,8 +27,8 @@ function getDepartment() {
                             $(this).hide();
                         }
                     }
-                    if (age != 'all') {
-                        if (rowAge == age) {
+                    if (dept != 'all') {
+                        if (rowDept == dept) {
                             $(this).show();
                         }
                         else {
@@ -65,52 +40,3 @@ function getDepartment() {
             });
         }
     }
-<select class="cl_country" id="ddlCountry">
-    <option value="all">Select Class </option>
-    <option value="India">india</option>
-    <option value="usa">usa</option>
-    <option value="uk">uk</option>
-</select>
-<select class="cl_age" id="ddlAge">
-    <option value="all">Select Class </option>
-    <option value="50">50</option>
-    <option value="60">60</option>
-    <option value="80">80</option>
-</select>
-<table style="width: 100%" id="table11" border="1">
-    <tr>
-        <th>name</th>
-        <th>country</th>
-        <th>Age</th>
-    </tr>
-    <tr>
-        <td>Jill</td>
-        <td>USA</td>
-        <td>50</td>
-    </tr>
-    <tr>
-        <td>Eve</td>
-        <td>UK</td>
-        <td>50</td>
-    </tr>
-    <tr>
-        <td>John</td>
-        <td>INDIA</td>
-        <td>80</td>
-    </tr>
-    <tr>
-        <td>sam</td>
-        <td>AUSTRALIA</td>
-        <td>80</td>
-    </tr>
-    <tr>
-        <td>joe</td>
-        <td>INDIA</td>
-        <td>60</td>
-    </tr>
-    <tr>
-        <td>alan</td>
-        <td>USA</td>
-        <td>60</td>
-    </tr>
-</table>
