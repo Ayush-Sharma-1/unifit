ba30f8359556bb50b7eba75fea77efc60cdb85d4
Ayush-Sharma-1 <2634592S@student.gla.ac.uk>
2022-03-22 11:43:22 +0000

    Registered the reddit module

--- uni_fit/admin.py
+++ uni_fit/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from uni_fit.models import UserProfile, Review, University, University_Department, Users, Post, Comment
+from uni_fit.models import UserProfile, Review, University, University_Department, Users, Post, Comment, Reddit
 
 admin.site.register(UserProfile)
 admin.site.register(Users)
@@ -8,3 +8,4 @@ admin.site.register(University_Department)
 admin.site.register(Review)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Reddit)
