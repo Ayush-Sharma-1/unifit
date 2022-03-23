c122d9df2e88cd9828c40a7adb92cb367242a105
Ayush-Sharma-1 <2634592S@student.gla.ac.uk>
2022-03-23 12:37:02 +0000

    Created test cases and also fixed css for university page

--- uni_fit/tests.py
+++ uni_fit/tests.py
@@ -1,3 +1,33 @@
+from urllib import response
 from django.test import TestCase
+from uni_fit.models import University, University_Department
+from django.urls import reverse
 
-# Create your tests here.
+
+class UniversityMethodTests(TestCase):
+    def test_value_in_university_rank(self):
+        university = University(UniName='Massachusetts Institute of Technology', Country='US', UniRank=1, About='The Massachusetts Institute of Technology is a private land-grant research university in Cambridge, Massachusetts.', Link='https://web.mit.edu/')
+        university.save()
+        self.assertEqual(university.UniRank >0,True)
+
+class UniversityDepartmentMethodTests(TestCase):
+    def test_value_in_university_rank(self):
+        university = University(UniName='Massachusetts Institute of Technology', Country='US', UniRank=1, About='The Massachusetts Institute of Technology is a private land-grant research university in Cambridge, Massachusetts.', Link='https://web.mit.edu/')
+        university.save()
+        universitydept = University_Department(UniName=University.objects.filter(pk=1).first(),DeptName='Engineering', DeptRank=1,Link='https://engineering.mit.edu/')
+        universitydept.save()
+        self.assertEqual(universitydept.DeptRank >0,True)
+
+class FavUniversityNameCheck(TestCase):
+    def test_fav_university_name(self):
+        #Dummy Data
+        university = University(UniName='Massachusetts Institute of Technology', Country='US', UniRank=1, About='The Massachusetts Institute of Technology is a private land-grant research university in Cambridge, Massachusetts.', Link='https://web.mit.edu/')
+        university.save()
+        universitydept = University_Department(UniName=University.objects.filter(pk=1).first(),DeptName='Engineering', DeptRank=1,Link='https://engineering.mit.edu/')
+        universitydept.save()
+        response = self.client.get(reverse('uni_fit:index'))
+        self.assertEqual(response.status_code, 200)
+
+
+
+
